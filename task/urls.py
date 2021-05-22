983d1fdbe1c65db56162d3f0fb1d6850524e5f5a
sarbesh <sarbeshsarkarsks@gmail.com>
2021-05-22 15:44:27 +0530

    CICD vuln fix

--- task/urls.py
+++ task/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-
 urlpatterns = [
     path('', views.TaskList.as_view(), name='all_tasks'),
-    path('<int:pk>', views.TaskDetails.as_view(), name = 'task_detail'),
+    path('<int:pk>', views.TaskDetails.as_view(), name='task_detail'),
     path('users', views.UserList.as_view()),
     path('users/<int:pk>', views.UserDetail.as_view())
 ]
